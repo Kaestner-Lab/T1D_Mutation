714f12c45320832906b63b7f2bd79611bf74f01a
Long Gao <gaolongnuaa@gmail.com>
2021-04-11 23:30:02 -0400

    Update HLA_typing.sh

--- HLA_typing.sh
+++ HLA_typing.sh
@@ -3,7 +3,7 @@
 # run novoalign for athlates
 novoalign -d athlates/Athlates_2014_04_26/db/ref/ref.nix -f R1.fq R2.fq -t 10 -o SAM -r all 100 -e 100 -i PE 200 140 | samtools view -bS -h -F 4 - > output.bam
 
-#run athlates
+# run athlates
 samtools view -b -L athlates/Athlates_2014_04_26/db/bed/HLA-*.bed sample.bam > HLA-*.bam      
 samtools view -h -o HLA-*.sam HLA-*.bam                                                                      
 grep -P "^@" HLA-*.sam > HLA-*.header                                                                        
@@ -26,5 +26,5 @@ athlates/Athlates_2014_04_26/bin/typing -bam HLA-*.sort.bam -exlbam non-HLA-*.ba
 # run PHLAT
 python -O PHLAT.py -1 R1.fq -2 R2.fq -index phlat-1.0/index4phlat -b2url bowtie2_dir/bowtie2 -tag sample -e phlat-1.0/ -p 8 -o output_dir
 
-#run optitype
+# run optitype
 python /OptiTypePipeline.py -i R1.fq R2.fq --dna --outdir output_dir
